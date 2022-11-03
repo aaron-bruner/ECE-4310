687c305e0ef4a251ea1faf7b33c5ad96615ba88d
Aaron Bruner <SkittlezExZ@gmail.com>
2022-11-02 21:18:02 -0400

    Basically no progress...

--- Lab 5/lab5.c
+++ Lab 5/lab5.c
@@ -22,10 +22,13 @@
 #define BLACK 0
 #define WHITE 255
 
+#define SQR(x) ((x)*(x))
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
+#include <math.h>
 #include <time.h>
 
 struct contourPoints {
@@ -34,6 +37,9 @@ struct contourPoints {
     int  y; // ROW
 };
 
+void sobel(unsigned char* gradientImage, unsigned char* sourceImage, int COLS, int ROWS);
+void normalize(unsigned char* normImage, unsigned char* srcImage, int max, int min, int COLS, int ROWS);
+void MaxMin(unsigned char* srcImage, int *max, int *min, int COLS, int ROWS);
 void outputImage(unsigned char* source, char* fileName, int col, int row);
 unsigned char* readImage(int* ROWS, int* COLS, char* source);
 unsigned char* createImage(int size);
@@ -45,10 +51,10 @@ char* contoursPointsDir = "hawk_init.txt";
 
 int main(int argc, char* argv[])
 {
-    unsigned char* sourceImage, *sourceWithContours;
+    unsigned char* sourceImage, *sourceWithContours, *gradientImage, *normalizedImage;
     struct contourPoints* contours;
 
-    int r, c, x, y, i = 0, j = 0, fileRows = 0, sourceROWS, sourceCOLS;
+    int r, c, x, y, i = 0, j = 0, fileRows = 0, sourceROWS, sourceCOLS, max = 0, min = 0;
 
     /* ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ */
     /*                 STEP 1: Read in source image and contour pixels                             */
@@ -72,6 +78,7 @@ int main(int argc, char* argv[])
     }
 
     /* ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ */
+    /*          STEP 2: Print plus signs on source image at contour locations                      */
     /* The image with the initial contours drawn as an arrow at each location
     *
     *  _   _   _   X-3 _   _   _
@@ -84,7 +91,7 @@ int main(int argc, char* argv[])
     *
     *  Create a copy of the original image */
     /* ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ */
-    sourceWithContours = createImage(sourceROWS * sourceCOLS);
+    sourceWithContours = createImage(sourceCOLS * sourceROWS);
     for (i = 0; i < sourceROWS * sourceCOLS; i++) sourceWithContours[i] = sourceImage[i];
     for (i = 0; i < fileRows; i++)
     {
@@ -97,12 +104,100 @@ int main(int argc, char* argv[])
     outputImage(sourceWithContours, "hawk_sourceArrows.ppm", sourceCOLS, sourceROWS);
 
     /* ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ */
+    /*                 STEP 3: Get the Sobel edge gradient magnitude image                         */
+    gradientImage = createImage(sourceCOLS * sourceROWS);
+    sobel(gradientImage, sourceImage, sourceCOLS, sourceROWS);
 
+    // Find maximum and minimum values
+    MaxMin(gradientImage, &max, &min, sourceCOLS, sourceROWS);
+
+    // Normalize the image using min and max values
+    normalizedImage = createImage(sourceROWS * sourceCOLS);
+    normalize(normalizedImage, gradientImage, max, min, sourceCOLS, sourceROWS);
+
+    outputImage(normalizedImage, "hawk_normalized.ppm", sourceCOLS, sourceROWS);
 
     /* ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ */
 
 
     return 0;
+}
+
+/// <summary>
+/// Normalize the source image using the maximum and minimum pixel values provided
+/// </summary>
+/// <param name="normImage"></param>
+/// <param name="srcImage"></param>
+/// <param name="max"></param>
+/// <param name="min"></param>
+/// <param name="COLS"></param>
+/// <param name="ROWS"></param>
+void normalize(unsigned char* normImage, unsigned char* srcImage, int max, int min, int COLS, int ROWS)
+{
+    for (int i = 0; i < COLS * ROWS; i++)
+        normImage[i] = (max == min == 0) ? 0 : (srcImage[i] - min) * 255 / (max - min);
+
+    return;
+}
+
+/// <summary>
+/// Find the maximum and minimum pixel value for the source image
+/// </summary>
+/// <param name="srcImage"></param>
+/// <param name="max"></param>
+/// <param name="min"></param>
+void MaxMin(unsigned char* srcImage, int *max, int *min, int COLS, int ROWS)
+{
+    int i = 0;
+
+    (*min) = (*max) = srcImage[0];
+
+    for (i = 0; i < COLS * ROWS; i++)
+    {
+        (*max) < srcImage[i] ? (*max) = srcImage[i] : (*max);
+        (*min) > srcImage[i] ? (*min) = srcImage[i] : (*min);
+    }
+    return;
+}
+
+/// <summary>
+/// 
+/// Level Edge | Vertical Edge | Sobel Template |
+/// -1  -2  -1 | -1   0   1    |  w1   w2   w3  |
+///  0   0   0 | -2   0   2    |  w4   w5   w6  |
+///  1   2   1 | -1   0   1    |  w7   w8   w9  |
+/// </summary>
+/// <param name="gradientImage"></param>
+/// <param name="sourceImage"></param>
+/// <param name="sourceROWS"></param>
+/// <param name="sourceCOLS"></param>
+void sobel(unsigned char* gradientImage, unsigned char* sourceImage, int COLS, int ROWS)
+{
+    int levelEdge[9]    = { -1, -2, -1,  0, 0, 0,  1, 2, 1 };
+    int verticalEdge[9] = { -1,  0,  1, -2, 0, 2, -1, 0, 1 };
+    int x, y, r, c, i, j;
+
+    // Copy source image into convolution image
+    for (i = 0; i < COLS * ROWS; i++) gradientImage[i] = sourceImage[i];
+
+    // Apply Sobel Filter
+    for (r = 1; r < ROWS - 1; r++)
+    {
+        for (c = 1; c < COLS - 1; c++, x = 0, y = 0)
+        {
+            for (i = -1; i <= 1; i++)
+            {
+                for (j = -1; j <= 1; j++)
+                {
+                    x +=    levelEdge[(i + 1) * 3 + (j + 1)] * sourceImage[(r + i) * COLS + (c + j)];
+                    y += verticalEdge[(i + 1) * 3 + (j + 1)] * sourceImage[(r + i) * COLS + (c + j)];
+                }
+            }
+            gradientImage[r * COLS + c] = sqrt(SQR(x) + SQR(y));
+        }
+    }
+
+    return;
 }
 
 /// <summary>
